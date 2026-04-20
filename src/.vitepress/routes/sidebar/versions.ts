8cc568adc5c8804936e67ff8cf086e25b8c6f0ab
Bruce Schultz <bschultz013@gmail.com>
2026-04-20 11:15:23 +0200

    docs(node): add migration guide and changes for v0.1.0

--- src/.vitepress/routes/sidebar/versions.ts
+++ src/.vitepress/routes/sidebar/versions.ts
@@ -3,4 +3,12 @@ export const versionRoutes = [
         text: '0.0.10',
         link: '/v0.0.10'
     },
+    {
+        text: '0.0.11',
+        link: '/v0.0.11'
+    },
+    {
+        text: '0.1.0',
+        link: '/v0.1.0'
+    },
 ]
