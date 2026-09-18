358ca8f9b88182f5de43e136a7770df19916af6c
Silva, Nayeli <nayelianaid.silvapreciado@uk-erlangen.de>
2026-09-18 14:33:27 +0200

    feat: add FAQs for Node management.

--- src/.vitepress/routes/sidebar/faqs.ts
+++ src/.vitepress/routes/sidebar/faqs.ts
@@ -2,5 +2,6 @@ export const faqsRoutes = [
     { text: 'Getting Started', link: '/faqs-of-getting-started'},
     { text: 'User: Proposal & Analysis', link: '/faqs-of-proposals-analysis'},
     { text: 'Admin: Hub Management', link: '/faqs-of-hub-management'},
+    { text: 'Admin: Node Management', link: '/faqs-of-node-management'},
     { text: 'Security & Privacy', link: '/faqs-of-security-privacy'},
 ]
